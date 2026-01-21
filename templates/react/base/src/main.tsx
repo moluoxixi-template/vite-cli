5fa7f46ff2b364d96a58cd8f6f74f950a24a95b5
moluoxixi <1983531544@qq.com>
2026-01-21 12:32:03 +0800

    feat: 补充ts配置

--- templates/react/base/src/main.tsx
+++ templates/react/base/src/main.tsx
@@ -6,8 +6,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { createRouter } from './router'
-import { setupFeatures } from './main/index'
+import { createRouter } from '@/router'
+import { setupFeatures } from '@/main/index'
 
 // Import styles
 import '@/assets/styles/main.scss'
